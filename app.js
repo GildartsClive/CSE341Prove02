c2ed4fd6069af4a7e75e901c43b2b2aa429980d7
GildartsClive <cmicmuir@gmail.com>
2022-01-15 18:10:56 -0700

    second commit

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('app started.');
-    next();
+//app.use((req, res, next) => {
+//    console.log('app started.');
+//    next();
+//});
+
+//app.use((req, res, next) => {
+//    console.log('app started.');
+//    res.send('<p>Assignment solved (almost!)</p>');
+//});
+
+app.use('/users', (req, res, next) => {
+    console.log('/users middleware');
+    res.send('<p>The /users handler.</p>');
 });
 
-app.use((req, res, next) => {
-    console.log('app started.');
-    res.send('<p>Assignment solved (almost!)</p>');
+
+app.use('/', (req, res, next) => {
+    console.log('/ middleware');
+    res.send('<p>The / handler.</p>');
 });
 
 app.listen(3000);
